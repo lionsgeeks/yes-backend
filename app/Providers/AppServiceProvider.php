324fd98146f5f9fa75aba8fbb4fe6bc3446adaf5
forkanimahdi <forkanimahdi@gmail.com>
2025-06-03 15:05:24 +0100

    fix bugs + deploying

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
+    { 
         $osc = Organization::all();
         view()->share([
             'osc' => $osc
